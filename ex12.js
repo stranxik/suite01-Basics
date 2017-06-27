6c679c66f3dac68b075221eb0c9549856bf8b3a5
Thibault Souris <stranxik@gmail.com>
2017-06-27 14:14:31 +0200

    maj

--- ex12.js
+++ ex12.js
@@ -73,3 +73,106 @@
 */
 
 // write your code below this comment
+
+
+
+
+
+
+function plus(number1, number2){
+    return number1 + number2;
+}
+function moins(number1, number2){
+    return number1 - number2;
+}
+function fois(number1, number2){
+    return number1 * number2;
+}
+function divi(number1, number2){
+    return number1 / number2;
+}
+function rest(number1, number2){
+    return number1 % number2;
+}
+
+function evalExpr(number1, number2, operator){
+    switch (true){        
+        case (operator === '+'):
+            return plus(number1, number2);
+            break;
+        case (operator === '-'):
+            return moins(number1, number2);
+            break;
+        case (operator === '*'):
+            return fois(number1, number2);
+            break;
+        case (number2 == 0 && operator === '/'):
+            return 0;
+            break;
+        case (operator === '/'):
+            return divi(number1, number2);
+            break;
+        case (operator === '%'):
+            return rest(number1, number2);
+            break;
+        default:
+            return "Erreur: entrez 2 chiffres et un operateur";
+            break;
+    }
+}
+/*
+    Vérification STEP 1
+ console.log(evalExpr(4, 5, "*")); // 20
+ console.log(evalExpr(3, 3, "+")); // 6
+ console.log(evalExpr(2, 2, "%")); // 0
+ console.log(evalExpr(3, 3, "-")); // 0
+ console.log(evalExpr(10, 2, "/")); // 5
+ console.log(evalExpr(10, 0, "/")); // 0
+*/
+
+ function getRandNum(){
+     return Math.floor(Math.random() * (900 - 100 + 1) + 100) ;
+ }
+ function getRandSign(){
+     return Math.floor(Math.random() * 5);
+ }
+
+ /*
+    Vérification STEP 2
+ console.log(getRandNum());
+ console.log(getSign());
+ */
+
+function generateCalculs(numberOfcalculs){
+    for(i = 0; i < numberOfcalculs; i++){
+        var nbre = getRandSign();
+        var number1 = getRandNum(); 
+        var number2 = getRandNum();
+        var sign = "";
+        var result = 0;
+        
+        switch (true){        
+            case (nbre === 0):
+                result = plus(number1, number2); 
+                sign = "+";
+                break;
+            case (nbre === 1):
+                result = moins(number1, number2); 
+                sign = "-";
+                break;
+            case (nbre === 2):
+                result = fois(number1, number2); 
+                sign = "*";
+                break;
+            case (nbre === 3):
+                result = divi(number1, number2); 
+                sign = "/";
+                break;
+            case (nbre === 4):
+                result = rest(number1, number2); 
+                sign = "%";
+                break;
+        }
+            var final = number1 + " " + sign + " " + number2 + " = " + result;
+    }
+};
